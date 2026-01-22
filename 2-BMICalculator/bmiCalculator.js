fc48d2c2bce129028227015d11e7bc0fb681b3a9
sanathecoder <sanasana33377737@gmail.com>
2026-01-22 20:28:03 +0500

    digital clock

--- 2-BMICalculator/bmiCalculator.js
+++ 2-BMICalculator/bmiCalculator.js
@@ -5,14 +5,23 @@ form.addEventListener('submit', function(e){
     const height =  parseInt(document.querySelector('#height').value);
     const weight = parseInt(document.querySelector('#weight').value);
     const result = document.querySelector('#results');
+    const res = document.querySelector('#res');
 
     if(height === "" || height < 0 || isNaN(height)){
-        result.innerHTML = " PLease give a valid number. "
+        result.innerHTML = " Please give a valid Height. "
     }else if(weight === "" || weight < 0 || isNaN(weight)){
-        result.innerHTML = "Please give a valid number. "
+        result.innerHTML = "Please give a valid Weight. "
     }else{
        const bmi = (weight/ (height*height/10000)).toFixed(2)
        // show result
        result.innerHTML = `<span>${bmi}</span>`
+       if(bmi < 18.6){
+        res.innerHTML = `<span>  Under Weight</span>`
+       }else if(bmi > 24.9){
+        res.innerHTML = `<span>Over Weight</span>`
+       }else {
+        res.innerHTML = ` <span>Normal Weight</span>`
+       }
     }
 })
+
